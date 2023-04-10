7e067cf6627230eee0ddd231e7efca4be06dadef
bluvar <0x317374@gmail.com>
2023-04-10 22:43:54 +0700

    chore(checkpoint): 4/8/2023, 1:54:14 PM GMT+0000

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -26,3 +26,36 @@ export const convertToSupportLang = (lang: string): Language => {
   }
   return lang as any;
 };
+
+export const defaultPrompts = [
+  {
+    name: "👾 code-assistant",
+    temperature: 0.2,
+    prompts: [
+      {
+        role: "system",
+        prompt:
+          "As an advanced chatbot named NullGPT, your primary goal is to assist users to write code. This may involve designing/writing/editing/describing code or providing helpful information. Where possible you should provide code examples to support your points and justify your recommendations or solutions. Make sure the code you provide is correct and can be run without errors. Be detailed and thorough in your responses. Your ultimate goal is to provide a helpful and enjoyable experience for the user. Write code inside markdown code block.",
+      },
+      "your",
+    ],
+    id: 1681141260062,
+  },
+  {
+    name: "🔠 translator-tech",
+    temperature: 0.2,
+    prompts: [
+      {
+        role: "system",
+        prompt:
+          "As an advanced chatbot named NullGPT, your primary goal is to translate to English, correct spelling and improve text sent by user. Your goal is to translate text, but not to change it's meaning. You can replace simplified A0-level words and sentences with more beautiful and elegant, upper level words and sentences. Keep the meaning same, but prioritize common, easy-to-understand words used in articles and documents on software programming. The topic I am talking about is programming, technical, software development, dev ops, game dev, backend, frontend, react, blockchain, aws, docker, unity engine or godot. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Write your answer inside markdown code block.",
+      },
+      "your",
+    ],
+    id: 1681141278150,
+  },
+];
+
+if (!localStorage.getItem(":prompts")) {
+  localStorage.setItem(":prompts", JSON.stringify(defaultPrompts));
+}
