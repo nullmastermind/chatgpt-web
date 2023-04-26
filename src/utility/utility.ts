0018318d1c6241f10a0c78883a1a18482824d9fa
bluvar <0x317374@gmail.com>
2023-04-26 15:27:29 +0700

    style(src/utility/utility.ts): improve preprocessMessageContent function

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -9,12 +9,13 @@ export type KeyValues = {
 };
 
 const preprocessMessageContent = (content: string) => {
-  // content = content.replace(/```(.*?)```/g, "```\n$1\n```");
+  content = content.replace(/```(.*?)```/g, "```\n$1\n```");
   const contentArr = content.split("\n").map(v => {
+    if (/[a-zA-Z0-9]/.test(v) && v.endsWith("```")) {
+      v = v.replace(/(.*?)```/g, "$1\n```");
+    }
     if (v.startsWith("```") && v.includes(" ")) {
       v = v.replace(/```(.*?)/g, "```\n$1");
-    } else if (/[a-zA-Z0-9]/.test(v) && v.endsWith("```")) {
-      v = v.replace(/(.*?)```/g, "$1\n```");
     }
     return v;
   });
