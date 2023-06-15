88e4d493a23caa5aba74922585e6ce6688a14404
bluvar <0x317374@gmail.com>
2023-06-15 16:42:22 +0700

    feat(api): add token encoding API endpoint

---
+++ src/pages/api/tokens.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+const { encode, decode } = require("gpt-3-encoder");
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const str = req.body.content || req.query.content || "";
+  const encoded = encode(str);
+  res.status(200).json({ data: encoded.length });
+}
