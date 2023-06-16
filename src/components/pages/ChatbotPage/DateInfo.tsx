899e6cdd8396c9126dd3ddee3447415cf33fcf71
bluvar <0x317374@gmail.com>
2023-06-16 21:13:27 +0700

    style(ChatbotPage): add margin top to DateInfo component

--- src/components/pages/ChatbotPage/DateInfo.tsx
+++ src/components/pages/ChatbotPage/DateInfo.tsx
@@ -28,7 +28,7 @@ const DateInfo = ({ message }: DateInfoProps) => {
 
   return (
     <>
-      <Text className={"opacity-60 text-sm"}>{info}</Text>
+      <Text className={"opacity-60 text-sm mt-1"}>{info}</Text>
     </>
   );
 };
