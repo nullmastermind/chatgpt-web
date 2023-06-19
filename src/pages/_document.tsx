d12236c7f5fcb7f177b9e947ae621fd5a8a9e1bb
bluvar <0x317374@gmail.com>
2023-06-19 13:17:55 +0700

    feat(GoogleTagManager): add Google Tag Manager component
    feat(_document): add Google Tag Manager script and noscript tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import GoogleTagManager from "@/components/GoogleTagManager";
 
 export default function Document() {
   return (
@@ -7,8 +8,17 @@ export default function Document() {
         <link rel="shortcut icon" href="/bot.png" />
         <script src="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/highlight.min.js"></script>
         <script>hljs.highlightAll();</script>
+        <GoogleTagManager />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TNT2FGS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
