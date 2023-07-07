d4cfb2a5eae7c13099e6fb893372576a0c44b3f7
bluvar <0x317374@gmail.com>
2023-07-07 15:00:28 +0700

    feat(package.json): add @vercel/analytics dependency + add Analytics component from @vercel/analytics/react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import dayjs from "dayjs";
+import { Analytics } from "@vercel/analytics/react";
 
 dayjs.extend(require("dayjs/plugin/relativeTime"));
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
