2f8ea27eed0ddd8ee9fe35977f3f33145abcbf4c
Van Dong <thenullmastermind@gmail.com>
2024-06-01 20:24:53 +0700

    feat: add 'isTyping' prop to MemoizedReactMarkdown component

--- src/components/pages/ChatbotPage/MemoizedReactMarkdown.tsx
+++ src/components/pages/ChatbotPage/MemoizedReactMarkdown.tsx
@@ -21,97 +21,100 @@ type MemoizedReactMarkdownProps = {
   id?: string;
   smallText?: boolean;
   isFirst?: boolean;
+  isTyping?: boolean;
 };
 
 const MAX_TEXT = 512;
 
-const MemoizedReactMarkdown = memo(({ content: _content, id, smallText, isFirst }: MemoizedReactMarkdownProps) => {
-  const { classes } = useStyles();
-  const [showAll, setShowAll] = useState(false);
-  const content = useMemo(() => {
-    if (isFirst && !showAll) {
-      return _content.substring(0, MAX_TEXT);
+const MemoizedReactMarkdown = memo(
+  ({ content: _content, id, smallText, isFirst, isTyping }: MemoizedReactMarkdownProps) => {
+    const { classes } = useStyles();
+    const [showAll, setShowAll] = useState(false);
+    const content = useMemo(() => {
+      if (isFirst && !showAll) {
+        return _content.substring(0, MAX_TEXT);
+      }
+
+      return _content;
+    }, [_content, isFirst, showAll]);
+
+    const md = (
+      <ReactMarkdown
+        linkTarget="_blank"
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          code({ node: rawNode, inline, className, children }) {
+            const node = rawNode as Node;
+
+            const rawContent = String(children);
+            let codeContent = postprocessAnswer(rawContent.replace(/\n$/, ""), true);
+
+            if (inline && !content.includes("```" + rawContent + "```")) {
+              if (node.position && node.position.end.offset - rawContent.length - node.position.start.offset === 2) {
+                return <code className={classes.inlineCode}>{codeContent}</code>;
+              }
+            }
+
+            const match = /language-(\w+)/.exec(className || "");
+            let lang = "javascript";
+
+            if (!match) {
+              try {
+                lang = detectProgramLang(codeContent);
+              } catch (e) {}
+            } else {
+              lang = match[1];
+            }
+
+            if (!inline && lang === "mermaid") {
+              return <MermaidDraw content={codeContent} />;
+            }
+
+            return (
+              <Prism
+                children={codeContent}
+                language={convertToSupportLang(lang)}
+                scrollAreaComponent={ScrollArea}
+                className={classNames("mb-1", classes.codeWrap)}
+              />
+            );
+          },
+        }}
+      >
+        {preprocessMessageContent(content)}
+      </ReactMarkdown>
+    );
+
+    if (smallText) {
+      return <div className={classNames("text-xs", classes.pBreakAll)}>{md}</div>;
     }
 
-    return _content;
-  }, [_content, isFirst, showAll]);
-
-  const md = (
-    <ReactMarkdown
-      linkTarget="_blank"
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
-      components={{
-        code({ node: rawNode, inline, className, children }) {
-          const node = rawNode as Node;
-
-          const rawContent = String(children);
-          let codeContent = postprocessAnswer(rawContent.replace(/\n$/, ""), true);
-
-          if (inline && !content.includes("```" + rawContent + "```")) {
-            if (node.position && node.position.end.offset - rawContent.length - node.position.start.offset === 2) {
-              return <code className={classes.inlineCode}>{codeContent}</code>;
-            }
-          }
-
-          const match = /language-(\w+)/.exec(className || "");
-          let lang = "javascript";
-
-          if (!match) {
-            try {
-              lang = detectProgramLang(codeContent);
-            } catch (e) {}
-          } else {
-            lang = match[1];
-          }
-
-          if (!inline && lang === "mermaid") {
-            return <MermaidDraw content={codeContent} />;
-          }
-
-          return (
-            <Prism
-              children={codeContent}
-              language={convertToSupportLang(lang)}
-              scrollAreaComponent={ScrollArea}
-              className={classNames("mb-1", classes.codeWrap)}
-            />
-          );
-        },
-      }}
-    >
-      {preprocessMessageContent(content)}
-    </ReactMarkdown>
-  );
-
-  if (smallText) {
-    return <div className={classNames("text-xs", classes.pBreakAll)}>{md}</div>;
-  }
-
-  return (
-    <div>
-      <div
-        className={classNames(classes.messageContent, classes.imgBg, {
-          "px-2 sm:px-0": isFirst,
-        })}
-      >
-        {md}
-      </div>
-      {isFirst && _content.length > MAX_TEXT && (
-        <Button
-          rightIcon={<IconMenuOrder size={"1rem"} />}
-          size={"xs"}
-          fullWidth={true}
-          className={classes.expandBox}
-          onClick={() => {
-            setShowAll(!showAll);
-          }}
+    return (
+      <div>
+        <div
+          className={classNames(classes.messageContent, classes.imgBg, {
+            "px-2 sm:px-0": isFirst,
+          })}
         >
-          Expand/Collapse
-        </Button>
-      )}
-    </div>
-  );
-});
+          {md}
+        </div>
+        {isFirst && _content.length > MAX_TEXT && (
+          <Button
+            rightIcon={<IconMenuOrder size={"1rem"} />}
+            size={"xs"}
+            fullWidth={true}
+            className={classes.expandBox}
+            onClick={() => {
+              setShowAll(!showAll);
+            }}
+          >
+            Expand/Collapse
+          </Button>
+        )}
+      </div>
+    );
+  }
+);
 
 export default MemoizedReactMarkdown;
