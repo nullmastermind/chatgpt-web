34853c38baf48db50d9aa03b9fb6678b8eeff0c1
Van Dong <thenullmastermind@gmail.com>
2024-06-02 10:11:27 +0700

    style: update layout and accessibility for UploadFile component in ChatbotPage

--- src/components/pages/ChatbotPage/UploadFile.tsx
+++ src/components/pages/ChatbotPage/UploadFile.tsx
@@ -13,45 +13,44 @@ import {
 const UploadFile = memo(() => {
   return (
     <div className="flex flex-row gap-2 items-center">
-      <div className={"text-sm font-bold"}>Add</div>
       <Menu>
         <Menu.Target>
-          <Button className={"p-0 h-auto"} variant={"default"} size={"xs"}>
-            <IconPlus size={"1rem"} />
+          <Button className={"p-0 px-1 h-auto"} variant={"default"} size={"xs"} rightIcon={<IconPlus size={"1rem"} />}>
+            Attach
           </Button>
         </Menu.Target>
         <Menu.Dropdown>
-          <Menu.Item className={"p-1"}>
+          <Menu.Item className={"p-1"} disabled={false}>
             <div className={"flex flex-row gap-1 items-center"}>
               <IconFileStack size={"1.3rem"} />
               <div>Private document</div>
             </div>
           </Menu.Item>
-          <Menu.Item className={"p-1"}>
+          <Menu.Item className={"p-1"} disabled={true}>
             <div className={"flex flex-row gap-1 items-center"}>
               <IconCsv size={"1.3rem"} />
               <div>Excel/Word/Text</div>
             </div>
           </Menu.Item>
-          <Menu.Item className={"p-1"}>
+          <Menu.Item className={"p-1"} disabled={true}>
             <div className={"flex flex-row gap-1 items-center"}>
               <IconPdf size={"1.3rem"} />
               <div>OCR - Pdf/Image</div>
             </div>
           </Menu.Item>
-          <Menu.Item className={"p-1"}>
+          <Menu.Item className={"p-1"} disabled={true}>
             <div className={"flex flex-row gap-1 items-center"}>
               <IconPhotoEdit size={"1.3rem"} />
               <div>Image</div>
             </div>
           </Menu.Item>
-          <Menu.Item className={"p-1"}>
+          <Menu.Item className={"p-1"} disabled={true}>
             <div className={"flex flex-row gap-1 items-center"}>
               <IconBrandShazam size={"1.3rem"} />
               <div>Audio</div>
             </div>
           </Menu.Item>
-          <Menu.Item className={"p-1"}>
+          <Menu.Item className={"p-1"} disabled={true}>
             <div className={"flex flex-row gap-1 items-center"}>
               <IconUnlink size={"1.3rem"} />
               <div>Website URL</div>
