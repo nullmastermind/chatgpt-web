6ee3eebaf857551e5bcf49ccaf2bced5483434f2
Van Dong <thenullmastermind@gmail.com>
2024-06-06 19:48:03 +0700

    refactor: improve mermaid chart rendering in ChatbotPage

--- src/components/pages/ChatbotPage/MermaidDraw.tsx
+++ src/components/pages/ChatbotPage/MermaidDraw.tsx
@@ -1,41 +1,38 @@
 import { memo, useEffect, useRef, useState } from "react";
 import { convertToSupportLang } from "@/utility/utility";
-import { ScrollArea, Tabs } from "@mantine/core";
+import { Card, ScrollArea, Tabs } from "@mantine/core";
 import classNames from "classnames";
 import { Prism } from "@mantine/prism";
 import useStyles from "@/components/pages/ChatbotPage/Message.style";
 import mermaid from "mermaid";
 import panzoom from "panzoom";
+import { v4 } from "uuid";
+
+mermaid.initialize({ startOnLoad: false, theme: "dark" });
 
 const MermaidDraw = memo<{
   content: string;
 }>(({ content }) => {
   const { classes } = useStyles();
   const [loading, setLoading] = useState(true);
-  const mermaidDockBlock = useRef<null>(null);
-
-  mermaid.initialize({ startOnLoad: false, theme: "dark" });
+  const mermaidDockBlock = useRef<HTMLDivElement>(null);
+  const [id, setId] = useState(v4());
 
   useEffect(() => {
-    if (!mermaidDockBlock.current) {
-      return;
-    }
-
-    setLoading(true);
-
-    // Render mermaid when mounted.
+    if (!mermaidDockBlock.current) return;
     mermaid
-      .run({
-        nodes: [mermaidDockBlock.current],
+      .render(id, content)
+      .then(value => {
+        mermaidDockBlock.current!.innerHTML = value.svg;
+        panzoom(mermaidDockBlock.current!, {});
+        setLoading(false);
       })
-      .then(() => setLoading(false));
-  });
-
-  useEffect(() => {
-    if (mermaidDockBlock.current) {
-      panzoom(mermaidDockBlock.current, {});
-    }
-  }, [mermaidDockBlock.current]);
+      .catch(e => {
+        if (e.toString().includes("is not a valid selector")) {
+          setId(v4());
+        }
+      });
+  }, [content, id]);
 
   return (
     <div className={"py-5"}>
@@ -45,15 +42,13 @@ const MermaidDraw = memo<{
           <Tabs.Tab value={"text"}>Text</Tabs.Tab>
         </Tabs.List>
         <Tabs.Panel value={"diagram"}>
-          <div className={"flex min-h-[256px] relative flex-row justify-center py-10 overflow-hidden"}>
-            <div className={"w-full"}>
-              <pre
+          <div className={"flex relative flex-row justify-center py-2 overflow-hidden"}>
+            <Card className={"w-full"}>
+              <div
                 ref={mermaidDockBlock}
                 className="w-full !p-0 whitespace-pre-wrap relative my-5 flex flex-row items-center justify-center"
-              >
-                {content}
-              </pre>
-            </div>
+              />
+            </Card>
             {loading && (
               <div className={"flex absolute top-0 left-0 w-full h-full items-center justify-center"}>
                 <div>Drawing diagram...</div>
