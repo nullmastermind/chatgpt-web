7fb08bb0e5b52ad595d006604bc72178d3e5b411
Van Dong <thenullmastermind@gmail.com>
2024-06-06 11:57:11 +0700

    Update yarn.lock with new package versions
    This commit updates yarn.lock with newer versions of various packages such as braintree, d3-scale, d3-time, among others, as well as introduces new ones like cose-base and cytoscape. This ensures that dependencies are up-to-date and introduces needed libraries to the project.

--- src/components/pages/ChatbotPage/MermaidDraw.tsx
+++ src/components/pages/ChatbotPage/MermaidDraw.tsx
@@ -1,10 +1,11 @@
-import { memo, useMemo, useState } from "react";
+import { memo, useEffect, useMemo, useRef, useState } from "react";
 import { convertToSupportLang } from "@/utility/utility";
 import { ScrollArea, Tabs } from "@mantine/core";
 import classNames from "classnames";
 import { Prism } from "@mantine/prism";
 import useStyles from "@/components/pages/ChatbotPage/Message.style";
 import { getMermaidImageUrl } from "@/components/misc/mermaid";
+import mermaid from "mermaid";
 
 const MermaidDraw = memo<{
   content: string;
@@ -14,6 +15,21 @@ const MermaidDraw = memo<{
     return getMermaidImageUrl(content);
   }, [content]);
   const [loading, setLoading] = useState(true);
+  const mermaidDockBlock = useRef<null>(null);
+  mermaid.initialize({ startOnLoad: false, theme: "dark" });
+
+  useEffect(() => {
+    if (!mermaidDockBlock.current) {
+      return;
+    }
+
+    // Render mermaid when mounted.
+    mermaid
+      .run({
+        nodes: [mermaidDockBlock.current],
+      })
+      .then(() => setLoading(false));
+  });
 
   return (
     <>
@@ -25,42 +41,20 @@ const MermaidDraw = memo<{
         <Tabs.Panel value={"diagram"}>
           <div
             style={{
-              minHeight: 400,
-              maxHeight: 400,
               maxWidth: "calc(100% - 50px)",
             }}
             className={"relative flex flex-row justify-center overflow-auto py-10"}
           >
             <div>
-              <img
-                onLoad={() => {
-                  setLoading(false);
-                }}
-                src={url}
-                alt={"Chart"}
-                className={"block !p-0 my-5"}
-              />
+              <pre ref={mermaidDockBlock} className="w-full !p-0 whitespace-pre-wrap relative my-5">
+                {content}
+              </pre>
             </div>
             {loading && (
               <div className={"flex absolute top-0 left-0 w-full h-full items-center justify-center"}>
                 <div>Drawing diagram...</div>
               </div>
             )}
-            {/*<iframe*/}
-            {/*  onLoad={() => {*/}
-            {/*    setTimeout(() => {*/}
-            {/*      setLoading(false);*/}
-            {/*    }, 300);*/}
-            {/*  }}*/}
-            {/*  src={url}*/}
-            {/*  style={{*/}
-            {/*    width: "100%",*/}
-            {/*    height: "100%",*/}
-            {/*  }}*/}
-            {/*  className={classNames("border-0", {*/}
-            {/*    "opacity-0": loading,*/}
-            {/*  })}*/}
-            {/*/>*/}
           </div>
         </Tabs.Panel>
         <Tabs.Panel value={"text"}>
