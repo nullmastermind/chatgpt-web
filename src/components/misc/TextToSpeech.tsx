86895124e565f8601a21724e3eddb3579c2c555a
Van Dong <thenullmastermind@gmail.com>
2024-06-20 18:40:39 +0700

    feat: add gender icons and local speech synthesis support to TextToSpeech component

--- src/components/misc/TextToSpeech.tsx
+++ src/components/misc/TextToSpeech.tsx
@@ -1,4 +1,5 @@
 import { Menu, Portal } from '@mantine/core';
+import { IconGenderBigender, IconGenderFemale, IconGenderMale } from '@tabler/icons-react';
 import axios from 'axios';
 import { ReactNode, memo, useEffect, useRef, useState } from 'react';
 
@@ -28,6 +29,20 @@ const TextToSpeech = memo<{
       autoPlayed.current = audioSrc;
       return;
     }
+
+    void audioRef.current?.pause();
+    window.speechSynthesis.cancel();
+
+    if (voiceId === 'local') {
+      if ('speechSynthesis' in window) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(utterance);
+      } else {
+        alert('Your browser does not support Text-to-Speech.');
+      }
+      return;
+    }
+
     setLoading(true);
     axios
       .post('/api/text-to-speech', {
@@ -53,6 +68,13 @@ const TextToSpeech = memo<{
     }
   }, [audioSrc]);
 
+  useEffect(() => {
+    return () => {
+      void audioRef.current?.pause();
+      window.speechSynthesis.cancel();
+    };
+  }, []);
+
   return (
     <>
       {audioSrc && (
@@ -65,23 +87,54 @@ const TextToSpeech = memo<{
         <Menu.Target>{children({ isLoading: loading })}</Menu.Target>
         <Portal>
           <Menu.Dropdown>
-            <Menu.Item className={'p-1'} onClick={() => play('nova')}>
-              Nova (female)
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('local')}
+              icon={<IconGenderBigender size={20} />}
+            >
+              Free
             </Menu.Item>
-            <Menu.Item className={'p-1'} onClick={() => play('shimmer')}>
-              Shimmer (female)
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('nova')}
+              icon={<IconGenderFemale size={20} />}
+            >
+              Nova
             </Menu.Item>
-            <Menu.Item className={'p-1'} onClick={() => play('onyx')}>
-              Onyx (male)
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('shimmer')}
+              icon={<IconGenderFemale size={20} />}
+            >
+              Shimmer
             </Menu.Item>
-            <Menu.Item className={'p-1'} onClick={() => play('fable')}>
-              Fable (male)
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('onyx')}
+              icon={<IconGenderMale size={20} />}
+            >
+              Onyx
             </Menu.Item>
-            <Menu.Item className={'p-1'} onClick={() => play('echo')}>
-              Echo (male)
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('fable')}
+              icon={<IconGenderMale size={20} />}
+            >
+              Fable
             </Menu.Item>
-            <Menu.Item className={'p-1'} onClick={() => play('alloy')}>
-              Alloy (male)
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('echo')}
+              icon={<IconGenderMale size={20} />}
+            >
+              Echo
+            </Menu.Item>
+            <Menu.Item
+              className={'p-1'}
+              onClick={() => play('alloy')}
+              icon={<IconGenderMale size={20} />}
+            >
+              Alloy
             </Menu.Item>
           </Menu.Dropdown>
         </Portal>
