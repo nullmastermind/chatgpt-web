2206f1b5cad589d8292f54f24ae001bac31cda42
Van Dong <thenullmastermind@gmail.com>
2024-09-30 01:16:35 +0700

    feat: Add dynamic model fetching to ModelSelect component
    - Import axios and lodash map
    - Add useState for modelOptions
    - Fetch models from API if baseUrl is set in localStorage
    - Update NativeSelect to use dynamic modelOptions

--- src/components/pages/ChatbotPage/ModelSelect.tsx
+++ src/components/pages/ChatbotPage/ModelSelect.tsx
@@ -1,5 +1,7 @@
 import { NativeSelect } from '@mantine/core';
-import { useEffect } from 'react';
+import axios from 'axios';
+import { map } from 'lodash';
+import { useEffect, useState } from 'react';
 
 import { useModel } from '@/states/states';
 
@@ -7,17 +9,34 @@ import models from '../../../utility/models.json';
 
 const ModelSelect = () => {
   const [model, setModel] = useModel();
+  const [modelOptions, setModelOptions] = useState(models);
 
   useEffect(() => {
     localStorage.setItem(':model', model);
   }, [model]);
+
+  useEffect(() => {
+    const baseUrl = localStorage.getItem(':overrideBaseUrl');
+    if (baseUrl) {
+      axios
+        .get(`${baseUrl}/models`)
+        .then(({ data }) => {
+          setModelOptions(
+            map(data.data, (item) => {
+              return { label: item.id, value: item.id, maxTokens: 100000, price: 0 } as any;
+            }),
+          );
+        })
+        .catch(Promise.resolve);
+    }
+  }, []);
 
   return (
     <>
       <NativeSelect
         value={model}
         size={'xs'}
-        data={models}
+        data={modelOptions}
         onChange={(e) => setModel(e.target.value)}
       />
     </>
